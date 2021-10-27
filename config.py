4e76e2707f933c2e880b6f674c5b0e742b3b236c
Dileepa_Malshan <84204637+Dileepa-Malshan@users.noreply.github.com>
2021-10-27 20:41:53 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+
+import os
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_USERNAME = os.getenv("BOT_USERNAME")
+IMAGE = os.getenv("IMAGE")
+MONGODB_URI = os.getenv("MONGODB_URI")
+LOG_CHANNEL = os.getenv("LOG_CHANNEL")
+BOT_OWNER = os.getenv("BOT_OWNER")
